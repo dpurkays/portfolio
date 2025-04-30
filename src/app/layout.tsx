eae58143e261bb4564a87cec779536d5dbadcd85
Dula Purkaystha <dpurkays@gmail.com>
2025-04-29 22:41:56 -0700

    Fix grammarly extension bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <div className={inter.className}>{children}</div>
+      </body>
     </html>
   );
 }
