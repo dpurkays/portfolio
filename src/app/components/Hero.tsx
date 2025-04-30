7ea03722cc7846e77606c61b605a7d8d1e14c26a
Dula Purkaystha <dpurkays@gmail.com>
2025-04-29 22:14:20 -0700

    Add explore button

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -2,9 +2,9 @@ import Fireflies from "./Fireflies"; // ✅ default import
 
 const Hero = () => {
   return (
-    <section className="relative w-full min-h-screen flex items-center justify-center px-4">
+    <section className="relative w-full min-h-screen md:min-h-[70vh] flex items-center justify-center px-4">
       <Fireflies />
-      <div className="text-center space-y-4 z-10">
+      <div className="flex flex-col items-center text-center space-y-4 z-10">
         <h1 className="text-3xl font-bold md:text-5xl">
           Hi, I'm <span className="text-accent1">Dula Purkaystha</span>
         </h1>
@@ -16,6 +16,13 @@ const Hero = () => {
         <p className="text-accent2 text-lg md:text-2xl">
           Grounded. Detail-driven. Always learning.
         </p>
+        <div className="p-[2px] rounded-lg bg-gradient-to-r from-accent1 to-accent2 md:w-3/5">
+          <a href="#about">
+            <div className="bg-background text-accent1 dark:text-accent2 rounded-lg px-6 py-3 font-medium flex items-baseline justify-center gap-2 hover:scale-[1.3] transition-transform duration-300">
+              Explore <span className="animate-bounce">↓</span>
+            </div>
+          </a>
+        </div>
       </div>
     </section>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,8 @@ import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main className="bg-background text-primary flex flex-col items-center justify-center px-4 overflow-hidden">
+    <main className="min-h-screen bg-background text-primary flex flex-col items-center justify-center px-4">
       <Hero />
-      <div className="text-center space-y-4 z-10">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-        vero minima, debitis aspernatur vitae fugit provident eaque. Dolor amet
-        obcaecati sequi sed similique fugit provident, corporis praesentium
-        doloribus, aperiam commodi?
-      </div>
     </main>
   );
 }
