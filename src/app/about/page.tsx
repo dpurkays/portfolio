d6f9c514f3c6ad3147cbf6c78262ab99ae4ddc90
Dula Purkaystha <dpurkays@gmail.com>
2025-04-30 20:31:06 -0700

    Add Tech Stack

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -20,26 +20,25 @@ export default function AboutPage() {
             <div className="flex flex-col gap-4 md:gap-6">
               <p>
                 I began my journey in Psychology, fascinated by how people think
-                and solve problems. But at some point, I felt lost—and that led
-                me to revisit my lifelong passion for technology. Studying
+                and solve problems. But at some point, I felt lost... and that
+                led me to revisit my lifelong passion for technology. Studying
                 Computer Science and completing BrainStation’s Software
                 Engineering Bootcamp helped me rebuild my path, this time
                 through real-world projects, hands-on learning, and
                 collaborative environments.
               </p>
               <p>
-                What I enjoy most about this field is the constant
-                evolution—there’s always something new to learn or improve.
-                Whether it’s refining a user experience, fixing a tricky bug, or
-                finding a more thoughtful way to approach a problem, I find joy
-                in the process. I'm known for being detail-oriented, eager to
-                grow, and motivated by the opportunity to turn ideas into
-                something real.
+                What I enjoy most about this field is the constant evolution;
+                there’s always something new to learn or improve. Whether it’s
+                refining a user experience, fixing a tricky bug, or finding a
+                more thoughtful way to approach a problem, I find joy in the
+                process. I'm known for being detail-oriented, eager to grow, and
+                motivated by the opportunity to turn ideas into something real.
               </p>
               <p>
                 The transition into tech wasn’t always easy, but it’s been
                 incredibly fulfilling. Outside of code, I like to get creative
-                with hobbies like crocheting and other hands-on projects—they
+                with hobbies like crocheting and other hands-on projects. They
                 help me think differently and recharge. I’m excited to keep
                 growing, collaborating, and creating work that makes a
                 meaningful impact.
@@ -49,8 +48,161 @@ export default function AboutPage() {
         </section>
         <section>
           <h2 id="about" className="section-title">
-            Tech Stack
+            Technical Skills
           </h2>
+          <div>
+            <section>
+              <h3 className="section-subheader">Languages</h3>
+              <div className="flex gap-1 flex-wrap">
+                <img
+                  src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&amp;logo=javascript&amp;logoColor=%23F7DF1E"
+                  alt="JavaScript"
+                />
+                <img
+                  src="https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&amp;logo=c&amp;logoColor=white"
+                  alt="C"
+                />
+                <img
+                  src="https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&amp;logo=c%2B%2B&amp;logoColor=white"
+                  alt="C++"
+                />
+                <img
+                  src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&amp;logo=openjdk&amp;logoColor=white"
+                  alt="Java"
+                />
+                <img
+                  src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&amp;logo=python&amp;logoColor=ffdd54"
+                  alt="Python"
+                />
+                <img
+                  src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&amp;logo=typescript&amp;logoColor=white"
+                  alt="TypeScript"
+                />
+              </div>
+            </section>
+            <section>
+              <h3 className="section-subheader">Frontend</h3>
+              <div className="flex gap-1 flex-wrap">
+                <img
+                  src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&amp;logo=html5&amp;logoColor=white"
+                  alt="HTML5"
+                />
+                <img
+                  src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&amp;logo=css3&amp;logoColor=white"
+                  alt="CSS3"
+                />
+                <img
+                  src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&amp;logo=react&amp;logoColor=%2361DAFB"
+                  alt="React"
+                />
+                <img
+                  src="https://img.shields.io/badge/Next-black?style=for-the-badge&amp;logo=next.js&amp;logoColor=white"
+                  alt="Next JS"
+                />
+                <img
+                  src="https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&amp;logo=SASS&amp;logoColor=white"
+                  alt="SASS"
+                />
+                <img
+                  src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&amp;logo=tailwind-css&amp;logoColor=white"
+                  alt="TailwindCSS"
+                />
+                <img
+                  src="https://img.shields.io/badge/MUI-%230081CB.svg?style=for-the-badge&amp;logo=mui&amp;logoColor=white"
+                  alt="MUI"
+                />
+                <img
+                  src="https://img.shields.io/badge/bootstrap-%238511FA.svg?style=for-the-badge&amp;logo=bootstrap&amp;logoColor=white"
+                  alt="Bootstrap"
+                />
+              </div>
+            </section>
+            <section>
+              <h3 className="section-subheader">Backend</h3>
+              <div className="flex gap-1 flex-wrap">
+                <img
+                  src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&amp;logo=node.js&amp;logoColor=white"
+                  alt="NodeJS"
+                />
+                <img
+                  src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&amp;logo=express&amp;logoColor=%2361DAFB"
+                  alt="Express.js"
+                />
+              </div>
+            </section>
+            <section>
+              <h3 className="section-subheader">Database</h3>
+              <div className="flex gap-1 flex-wrap">
+                <img
+                  src="https://img.shields.io/badge/mysql-4479A1.svg?style=for-the-badge&amp;logo=mysql&amp;logoColor=white"
+                  alt="MySQL"
+                />
+                <img
+                  src="https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&amp;logo=mongodb&amp;logoColor=white"
+                  alt="MongoDB"
+                />
+              </div>
+            </section>
+            <section>
+              <h3 className="section-subheader">Data Science</h3>
+              <div className="flex gap-1 flex-wrap">
+                <img
+                  src="https://img.shields.io/badge/chart.js-F5788D.svg?style=for-the-badge&amp;logo=chart.js&amp;logoColor=white"
+                  alt="Chart.js"
+                />
+                <img
+                  src="https://img.shields.io/badge/numpy-%23013243.svg?style=for-the-badge&amp;logo=numpy&amp;logoColor=white"
+                  alt="NumPy"
+                />
+                <img
+                  src="https://img.shields.io/badge/pandas-%23150458.svg?style=for-the-badge&amp;logo=pandas&amp;logoColor=white"
+                  alt="Pandas"
+                />
+                <img
+                  src="https://img.shields.io/badge/Matplotlib-%23ffffff.svg?style=for-the-badge&amp;logo=Matplotlib&amp;logoColor=black"
+                  alt="Matplotlib"
+                />
+                <img
+                  src="https://img.shields.io/badge/scikit--learn-%23F7931E.svg?style=for-the-badge&amp;logo=scikit-learn&amp;logoColor=white"
+                  alt="scikit-learn"
+                />
+              </div>
+            </section>
+            <section>
+              <h3 className="section-subheader">Tools</h3>
+              <div className="flex gap-1 flex-wrap">
+                <img
+                  src="https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&amp;logo=git&amp;logoColor=white"
+                  alt="Git"
+                />
+                <img
+                  src="https://img.shields.io/badge/gitlab-%23181717.svg?style=for-the-badge&amp;logo=gitlab&amp;logoColor=white"
+                  alt="GitLab"
+                />
+                <img
+                  src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&amp;logo=github&amp;logoColor=white"
+                  alt="GitHub"
+                />
+                <img
+                  src="https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&amp;logo=postman&amp;logoColor=white"
+                  alt="Postman"
+                />
+                <img
+                  src="https://img.shields.io/badge/jira-%230A0FFF.svg?style=for-the-badge&amp;logo=jira&amp;logoColor=white"
+                  alt="Jira"
+                />
+              </div>
+            </section>
+            <section>
+              <h3 className="section-subheader">Others</h3>
+              <div className="flex gap-1 flex-wrap">
+                <img
+                  src="https://img.shields.io/badge/Canva-%2300C4CC.svg?style=for-the-badge&amp;logo=Canva&amp;logoColor=white"
+                  alt="Canva"
+                />
+              </div>
+            </section>
+          </div>
         </section>
         <section>
           <h2 id="about" className="section-title">
