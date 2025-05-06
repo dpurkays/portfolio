72201a467aeac77c290cbf25c37a482be6c52429
Dula Purkaystha <dpurkays@gmail.com>
2025-05-05 20:42:03 -0700

    Add hamburger menu on smaller screens

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,4 +1,7 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
+import HamburgerIcon from "./HamburgerIcon";
 
 type NavItem = {
   label: string;
@@ -6,6 +9,8 @@ type NavItem = {
 };
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   const navItems: NavItem[] = [
     { label: "Home", href: "/" },
     { label: "About", href: "#about" },
@@ -18,13 +23,24 @@ function Header() {
 
   return (
     <header className="fixed top-0 left-0 w-full shadow-md z-50">
-      <div className="flex justify-between items-center bg-accent2 gap-4 px-4 py-4 md:px-6">
+      <div className="flex justify-between items-center bg-background gap-4 px-4 py-4 md:px-6">
         <div className="text-primary font-bold text-xl ">Dula P</div>
-        <nav>
+        <div
+          className="lg:hidden text-primary focus:outline-none"
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label="Toggle menu"
+        >
+          <HamburgerIcon />
+        </div>
+
+        <nav className="hidden lg:flex">
           <ul className="flex space-x-6 text-primary font-medium text-sm">
             {navItems.map(({ label, href }) => (
               <li key={label}>
-                <Link href={href} className="hover:underline cursor-pointer">
+                <Link
+                  href={href}
+                  className="block px-4 py-2 rounded transition active:bg-accent1 cursor-pointer"
+                >
                   {label}
                 </Link>
               </li>
@@ -32,6 +48,24 @@ function Header() {
           </ul>
         </nav>
       </div>
+
+      {isOpen && (
+        <nav className="lg:hidden absolute top-full left-0 w-full bg-background">
+          <ul className="flex flex-col items-center py-4 space-y-2 text-primary font-medium text-sm">
+            {navItems.map(({ label, href }) => (
+              <li key={label}>
+                <Link
+                  href={href}
+                  onClick={() => setIsOpen(false)}
+                  className="block px-4 py-2 rounded transition active:bg-accent1"
+                >
+                  {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
     </header>
   );
 }
