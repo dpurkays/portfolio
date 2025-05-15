bc44d93e72ee0a1e8a9ecc92a4cad5ee006d7667
Dula Purkaystha <dpurkays@gmail.com>
2025-05-15 12:24:30 -0700

    Centerize h2

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <div id="contact" className="scroll-mt-24 w-full">
       <section className="section-style">
-        <h2 className="section-title pb-4 text-center">Get In Touch</h2>
+        <h2 className="section-title pb-4">Get In Touch</h2>
         <p className="text-center text-muted max-w-2xl mx-auto mb-6">
           Open to meaningful collaborations, new opportunities, or just a
           friendly hello. Feel free to reach out. I’d love to hear from you!
