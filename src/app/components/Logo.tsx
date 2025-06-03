227639d90fdbe729e6d30dab0c711b808ae4a910
Dula Purkaystha <dpurkays@gmail.com>
2025-06-03 16:42:28 -0700

    Change logo

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -6,8 +6,8 @@ function Logo() {
   return (
     <Link href="/">
       <div className="flex gap-2 items-center">
-        <Image src={logo} alt="logo" width={40} height={40} />
-        <div className="text-accent1 font-bold text-xl md:text-2xl hover:text-accent2">{`<Dula P />`}</div>
+        <Image src={logo} alt="logo" width={90} height={70} />
+        {/* <div className="text-accent1 font-bold text-xl md:text-2xl hover:text-accent2">{`<Dula P />`}</div> */}
       </div>
     </Link>
   );
