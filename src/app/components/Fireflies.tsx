7f94a3cb6d3983472573548c3cadfc8309752c6e
Dula Purkaystha <dpurkays@gmail.com>
2025-06-11 15:27:17 -0700

    remove eslint for particles

--- src/app/components/Fireflies.tsx
+++ src/app/components/Fireflies.tsx
@@ -6,6 +6,7 @@ import { loadFull } from "tsparticles";
 import firefliesConfig from "../../config/fireflies.config";
 
 export default function Fireflies() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const particlesInit = useCallback(async (engine: any) => {
     await loadFull(engine);
   }, []);
