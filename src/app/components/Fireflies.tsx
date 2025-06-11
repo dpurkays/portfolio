4d4c6c078af985e713a056af99b886bcdbf32f4a
Dula Purkaystha <dpurkays@gmail.com>
2025-06-11 15:09:57 -0700

    Fix ts particles build error

--- src/app/components/Fireflies.tsx
+++ src/app/components/Fireflies.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Particles from "react-tsparticles";
+import Particles from "@tsparticles/react";
 import { loadFull } from "tsparticles";
 import { Engine } from "tsparticles-engine";
 import firefliesConfig from "../../config/fireflies.config";
