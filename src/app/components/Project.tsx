fd377555863d0a1348bd301dc3ca02a50ea45f20
Dula Purkaystha <dpurkays@gmail.com>
2025-08-11 16:54:01 -0700

    Change grid to show 2 cols per row

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -16,7 +16,7 @@ function Project() {
         </motion.div>
       )}
       <motion.div
-        className="grid grid-cols-1 md:grid-cols-3 gap-6 pt-6"
+        className="grid grid-cols-1 md:grid-cols-2 gap-6 pt-6"
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true, amount: 0.2 }}
